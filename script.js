62e5121b213c9ac971fb5a21f387d728404aa256
Dillon <181585418+Velarken@users.noreply.github.com>
2025-06-06 14:21:01 -0400

    Finished code refactor for OOP, added player specific action and economy trackers

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-let d4Count = 0;
-let d6Count = 0;
-let d8Count = 0;
-let d10Count = 0;
-let d12Count = 0;
-let d20Count = 0;
-let d100Count = 0;
 let rolledAmount = 0;
 
-const resetDice = document.querySelector(".reset-dice");
+const resetDiceButton = document.querySelector(".reset-dice");
 // Plus Buttons
 const d4PlusButton = document.querySelector(".d4plus");
 const d6PlusButton = document.querySelector(".d6plus");
@@ -63,16 +56,16 @@ d20PlusButton.addEventListener('click', () =>  modifyDiceCount(5,validDice, "plu
 d20MinusButton.addEventListener('click', () =>  modifyDiceCount(5,validDice, "minus"));
 d100PlusButton.addEventListener('click', () =>  modifyDiceCount(6,validDice, "plus"));
 d100MinusButton.addEventListener('click', () =>  modifyDiceCount(6,validDice, "minus"));
-resetDice.addEventListener('click', () => resetAllDice());
+resetDiceButton.addEventListener('click', () => resetDice());
 
 // Roll Dice Event Listeners
-d4RollDice.addEventListener('click', () => rollAllDice(validDice[0].count,"d4"));
-d6RollDice.addEventListener('click', () => rollAllDice(validDice[1].count,"d6"));
-d8RollDice.addEventListener('click', () => rollAllDice(validDice[2].count,"d8"));
-d10RollDice.addEventListener('click', () => rollAllDice(validDice[3].count,"d10"));
-d12RollDice.addEventListener('click', () => rollAllDice(validDice[4].count,"d12"));
-d20RollDice.addEventListener('click', () => rollAllDice(validDice[5].count,"d20"));
-d100RollDice.addEventListener('click', () => rollAllDice(validDice[6].count,"d100"));
+d4RollDice.addEventListener('click', () => rollDice(validDice[0].count,validDice[0].key));
+d6RollDice.addEventListener('click', () => rollDice(validDice[1].count,validDice[1].key));
+d8RollDice.addEventListener('click', () => rollDice(validDice[2].count,validDice[2].key));
+d10RollDice.addEventListener('click', () => rollDice(validDice[3].count,validDice[3].key));
+d12RollDice.addEventListener('click', () => rollDice(validDice[4].count,validDice[4].key));
+d20RollDice.addEventListener('click', () => rollDice(validDice[5].count,validDice[5].key));
+d100RollDice.addEventListener('click', () => rollDice(validDice[6].count,validDice[6].key));
 
 // Fear
 const fear1 = document.querySelector(".fear-counter1");
@@ -84,15 +77,17 @@ fear1.addEventListener('click', () => markFear(fear1));
 fear2.addEventListener('click', () => markFear(fear2));
 fear3.addEventListener('click', () => markFear(fear3));
 
+/* 
+    Somehow dice dont roll if there is only a count of 1
+*/
 
-// Refactor functions to use the dice object instead of the variables above
-const d4 = {name:"d4",sides:4,count:0,key:0,selectUp:d4PlusButton,selectDown:d4MinusButton,display:d4CountDisplay};
-const d6 = {name:"d6",sides:6,count:0,key:1,selectUp:d6PlusButton,selectDown:d6MinusButton,display:d6CountDisplay};
-const d8 = {name:"d8",sides:8,count:0,key:2,selectUp:d8PlusButton,selectDown:d8MinusButton,display:d8CountDisplay};
-const d10 = {name:"d10",sides:10,count:0,key:3,selectUp:d10PlusButton,selectDown:d10MinusButton,display:d10CountDisplay};
-const d12 = {name:"d12",sides:12,count:0,key:4,selectUp:d12PlusButton,selectDown:d12MinusButton,display:d12CountDisplay};
-const d20 = {name:"d20",sides:20,count:0,key:5,selectUp:d20PlusButton,selectDown:d20MinusButton,display:d20CountDisplay};
-const d100 = {name:'d100',sides:100,count:0,key:6,selectUp:d100PlusButton,selectDown:d100MinusButton,display:d100CountDisplay};
+const d4 = {name:"d4",sides:4,count:1,key:0,display:d4CountDisplay,output:d4rollOutput};
+const d6 = {name:"d6",sides:6,count:1,key:1,display:d6CountDisplay,output:d6rollOutput};
+const d8 = {name:"d8",sides:8,count:1,key:2,display:d8CountDisplay,output:d8rollOutput};
+const d10 = {name:"d10",sides:10,count:1,key:3,display:d10CountDisplay,output:d10rollOutput};
+const d12 = {name:"d12",sides:12,count:1,key:4,display:d12CountDisplay,output:d12rollOutput};
+const d20 = {name:"d20",sides:20,count:1,key:5,display:d20CountDisplay,output:d20rollOutput};
+const d100 = {name:'d100',sides:100,count:1,key:6,display:d100CountDisplay,output:d100rollOutput};
 
 const validDice = [d4,d6,d8,d10,d12,d20,d100];
 
@@ -105,72 +100,25 @@ function modifyDiceCount(key,array,modification) {
             array[key].display.textContent = array[key].count;
         }
 }
-
-function rollAllDice (number, type) {
-    if (!isNaN(number) && number > 0) {
-        if (type === "d4") {
-            for (let i = 1; i <= number; i++) {
-                rolledAmount+= Math.floor(Math.random()*4+1);
-                d4rollOutput.textContent = rolledAmount;
-            }
-        } else if (type === "d6") {
-            for (let i = 1; i <= number; i++) {
-                rolledAmount+= Math.floor(Math.random()*6+1);
-                d6rollOutput.textContent = rolledAmount;
-            }
-        } else if (type === "d8") {
-            for (let i = 1; i <= number; i++) {
-                rolledAmount+= Math.floor(Math.random()*8+1);
-                d8rollOutput.textContent = rolledAmount;
-            }
-        } else if (type === "d10") {
-            for (let i = 1; i <= number; i++) {
-                rolledAmount+= Math.floor(Math.random()*10+1);
-                d10rollOutput.textContent = rolledAmount;
-            }
-        } else if (type === "d12") {
-            for (let i = 1; i <= number; i++) {
-                rolledAmount+= Math.floor(Math.random()*12+1);
-                d12rollOutput.textContent = rolledAmount;
-            }
-        }else if (type === "d20") {
-            for (let i = 1; i <= number; i++) {
-                rolledAmount+= Math.floor(Math.random()*20+1);
-                d20rollOutput.textContent = rolledAmount;
-            }
-        } else if (type === "d100") {
-            for (let i = 1; i <= number; i++) {
-                rolledAmount+= Math.floor(Math.random()*100+1);
-                d100rollOutput.textContent = rolledAmount;
-            }
+function rollDice(diceToRoll,key) {
+    if (validDice[key].count < 1) {
+        alert("Can't roll negative dice!")
+    } else {
+        for (let i = 0; i < diceToRoll; i++) {
+            rolledAmount += Math.floor(Math.random()*validDice[key].sides+1);
+            validDice[key].output.textContent = rolledAmount;
         }
     }
-    rolledAmount=0;
+    rolledAmount = 0;
 }
-function resetAllDice() {
-    d4Count=0;
-    d6Count=0;
-    d8Count=0;
-    d10Count=0;
-    d12Count=0;
-    d20Count=0;
-    d100Count=0;
-    rolledAmount=0;
-    d4CountDisplay.textContent = 0;
-    d6CountDisplay.textContent = 0;
-    d8CountDisplay.textContent = 0;
-    d10CountDisplay.textContent = 0;
-    d12CountDisplay.textContent = 0;
-    d20CountDisplay.textContent = 0;
-    d100CountDisplay.textContent = 0;
-    d4rollOutput.textContent = 0;
-    d6rollOutput.textContent = 0;
-    d8rollOutput.textContent = 0;
-    d10rollOutput.textContent = 0;
-    d12rollOutput.textContent = 0;
-    d20rollOutput.textContent = 0;
-    d100rollOutput.textContent = 0;
+function resetDice() {
+    for (key in validDice) {
+        validDice[key].count = 1;
+        validDice[key].display.textContent = 1;
+        validDice[key].output.textContent = "Total Rolled:  0";
+    }
 }
+
 function markFear(button) {
     if (button.style.backgroundColor = "purple") {
         button.style.backgroundColor = "yellow";
@@ -180,60 +128,3 @@ function markFear(button) {
     }
     // When clicked, the .fear-counter div will change color to denote that it is marked active.
 }
-
-// Depreciated Code
-
-/* 
-function diceCount(die,modifier) {
-    if (modifier === "plus") {
-        if (die === "d4") {
-            d4Count++;
-            d4CountDisplay.textContent = d4Count;
-        } else if (die === "d6") {
-            d6Count++;
-            d6CountDisplay.textContent = d6Count;
-        } else if (die === "d8") {
-            d8Count+=1
-            d8CountDisplay.textContent = d8Count;
-        } else if (die === "d10") {
-            console.log("hi")
-            d10Count+=1
-            d10CountDisplay.textContent = d10Count;
-        } else if (die === "d12") {
-            d12Count+=1
-            d12CountDisplay.textContent = d12Count;
-        }else if (die === "d20") {
-            d20Count+=1
-            d20CountDisplay.textContent = d20Count;
-        } else if (die === "d100") {
-            d100Count+=1
-            d100CountDisplay.textContent = d100Count;
-        }
-    } else if (modifier === "minus") {
-        if (die === "d4") {
-            d4Count-=1
-            d4CountDisplay.textContent = d4Count;
-        } else if (die === "d6") {
-            d6Count-=1
-            d4CountDisplay.textContent = d6Count;
-        } else if (die === "d8") {
-            d8Count-=1
-            d8CountDisplay.textContent = d8Count;
-        } else if (die === "d10") {
-            d10Count-=1
-            d10CountDisplay.textContent = d10Count;
-        } else if (die === "d12") {
-            d12Count-=1
-            d12CountDisplay.textContent = d12Count;
-        }else if (die === "d20") {
-            d20Count-=1
-            d20CountDisplay.textContent = d20Count;
-        } else if (die === "d100") {
-            d100Count-=1
-            d100CountDisplay.textContent = d100Count;
-        }
-    } else {
-        throw console.error("This is not accepted by the function");
-    }
-}
-*/
